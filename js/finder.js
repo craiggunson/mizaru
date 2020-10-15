2352bf5c66dd4a97a6ee8a9e1a96b2603775f723
Craig Gunson <cbgunson@gmail.com>
2020-10-16 09:56:39 +1100

    working hidden video

--- js/finder.js
+++ js/finder.js
@@ -17,8 +17,8 @@ function snapshot(stream) {
   localMediaStream = stream;
   console.log('videoWidth:', video.videoWidth, 'videoHeight', video.videoHeight);
   ctx.drawImage(video, 0, 0, 1000, 1000, 0, 0, 300, 300);
-  document.querySelector('somedata').src = canvas.toDataURL('image/png');
-  var image_data = document.querySelector('somedata').src;
+  document.getElementById('somedata').src = canvas.toDataURL('image/png');
+  var image_data = document.getElementById('somedata').src;
   var imageBlob = dataURItoBlob(image_data);
   var params = {
     Image: {
