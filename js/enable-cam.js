797e309b7d27d3d112b4fafb466e96567a104c2f
Craig Gunson <cbgunson@gmail.com>
2020-10-16 11:47:14 +1100

    style

--- js/enable-cam.js
+++ js/enable-cam.js
@@ -14,7 +14,7 @@ function gotDevices(deviceInfos) {
     var option = document.createElement('option');
     option.value = deviceInfo.deviceId;
     if (deviceInfo.kind === 'videoinput') {
-      option.text = 'camera ' +
+      option.text = 'c' +
         (videoSelect.length + 1);
       videoSelect.appendChild(option);
     } else {
